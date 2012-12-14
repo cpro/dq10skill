ec4366bc47eb4222d3268b338f7c8d405a262f69
s w <sw@example.com>
2012-12-14 20:20:18 +0900

    asinリストをasin_list.txtに分離

--- produce_index.rb
+++ produce_index.rb
@@ -3,10 +3,10 @@ require 'haml'
 require 'json'
 
 data = JSON.parse(File.read('dq10skill-data.json'))
-
+asin_list = File.readlines('asin_list.txt').map { |e| e.chomp }
 #skillCategories = data['skillCategories']
 #data['vocationOrder'].each do |vocation|
 #  puts vocation
 #  puts data['vocations'][vocation]['skills'].join
 #end
-puts Haml::Engine.new(File.read('index.haml')).render(Object.new, :data => data)
+puts Haml::Engine.new(File.read('index.haml')).render(Object.new, {:data => data, :asin_list => asin_list})
