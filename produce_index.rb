a276436ca51ffa8364eecab399bcaab94cf9f07f
s w <cpro2918@gmail.com>
2012-12-20 16:56:41 +0900

    Amazonの広告表示の公式ウィジェット読込廃止。HAMLからの生成時、Ruby/AWSで画像・タイトル等を取得して静的なリンクを作成するように変更。

--- produce_index.rb
+++ produce_index.rb
@@ -3,12 +3,44 @@
 require 'rubygems'
 require 'haml'
 require 'json'
+require 'amazon/aws'
+require 'amazon/aws/search'
+
+# Ruby/AWS 設定
+ENV['AMAZONRCDIR']  = '.'
+ENV['AMAZONRCFILE'] = '.amazonrc'
+include Amazon::AWS
+
+asin_list = File.readlines('asin_list.txt').map { |e| e.chomp }
+item_list = asin_list.map do |asin|
+  il = ItemLookup.new('ASIN', {ItemId: asin})
+  request = Search::Request.new
+  
+  retry_count = 0
+  begin
+    response = request.search il
+  rescue HTTPError => e
+    retry_count += 1
+    if retry_count < 5
+      STDOUT.puts "retrying #{asin} ..."
+      sleep 1
+      retry
+    else
+      raise e
+    end
+  end
+  
+  item = response.item_lookup_response.items.item
+  {
+    img_url: item.medium_image.url,
+    img_height: item.medium_image.height,
+    img_width: item.medium_image.width,
+    item_url: item.detail_page_url,
+    item_title: item.item_attributes.title
+  }
+end
 
 data = JSON.parse(File.read('dq10skill-data.json'))
-asin_list = File.readlines('asin_list.txt').map { |e| e.chomp }
-#skillCategories = data['skillCategories']
-#data['vocationOrder'].each do |vocation|
-#  puts vocation
-#  puts data['vocations'][vocation]['skills'].join
-#end
-puts Haml::Engine.new(File.read('index.haml')).render(Object.new, {:data => data, :asin_list => asin_list})
+File.open('index.html', 'w') do |file|
+  file.puts Haml::Engine.new(File.read('index.haml')).render(Object.new, {:data => data, :item_list => item_list})
+end
