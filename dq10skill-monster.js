2ecfd1a76bc402c74ff76e294f7808b686a339fc
s w <cpro2918@gmail.com>
2013-12-02 15:16:52 +0900

    Monsterプロトタイプ実装

--- dq10skill-monster.js
+++ dq10skill-monster.js
@@ -3,9 +3,10 @@ var Simulator = (function() {
 	var SKILL_PTS_MAX = 40;
 	var LEVEL_MIN = 1;
 	var LEVEL_MAX = 30;
+	var MONSTER_MAX = 8;
 
 	var DATA_JSON_URI = window.location.href.substring(0, window.location.href.lastIndexOf('/') + 1) + 'dq10skill-monster-data.json';
-	
+
 	//データロード
 	var allData;
 	$.ajaxSetup({async: false});
@@ -25,64 +26,105 @@ var Simulator = (function() {
 	var skillPts = {};
 	var levels = {};
 	var monsters = [];
-
-	//パラメータ初期化
-	for(var monster in monsters) {
-		skillPts[monster] = {};
-		for(var s = 0; s < monsters[monster].skills.length; s++) {
-			var skillCategory = monsters[monster].skills[s];
-			skillPts[monster][skillCategory] = 0;
-		}
-		levels[monster] = LEVEL_MIN;
-	}
 	
-	/* メソッド */
-	//新規モンスターオブジェクト
-	function newMonster(monsterName) {
-		var monsterData = monsterList[monsterName];
-		var monster = {
-			name: monsterName
-			level: LEVEL_MIN,
-			skillPts = {}
-		};
+	/*モンスターオブジェクト */
 
-		for(var s = 0; s < monsterData.skills.length; s++) {
-			monster.skillPts[monsterData.skills[s]] = 0;
+	//コンストラクタ
+	function Monster (monsterType) {
+		this.data = monsterList[monsterType];
+		this.monsterType = monsterType;
+		this.level = LEVEL_MIN;
+		this.skillPts = {};
+
+		for(var s = 0; s < this.data.skills.length; s++) {
+			this.skillPts[this.data.skills[s]] = 0;
 		}
 	}
+
 	//スキルポイント取得
-	function getSkillPt(vocation, skillCategory) {
-		return skillPts[vocation][skillCategory];
-	}
+	Monster.prototype.getSkillPt = function(skillCategory) {
+		return this.skillPts[skillCategory];
+	};
 	
 	//スキルポイント更新：不正値の場合falseを返す
-	function updateSkillPt(vocation, skillCategory, newValue) {
-		var oldValue = skillPts[vocation][skillCategory];
+	Monster.prototype.updateSkillPt = function(skillCategory, newValue) {
+		var oldValue = this.skillPts[skillCategory];
 		if(newValue < SKILL_PTS_MIN || newValue > SKILL_PTS_MAX) {
 			return false;
 		}
-		if(totalOfSameSkills(skillCategory) - oldValue + newValue > SKILL_PTS_MAX) {
-			return false;
-		}
 		
-		skillPts[vocation][skillCategory] = newValue;
+		this.skillPts[skillCategory] = newValue;
 		return true;
-	}
+	};
 	
 	//レベル値取得
-	function getLevel(vocation) {
-		return levels[vocation];
-	}
+	Monster.prototype.getLevel = function() {
+		return this.level;
+	};
 	
 	//レベル値更新
-	function updateLevel(vocation, newValue) {
-		var oldValue = levels[vocation];
+	Monster.prototype.updateLevel = function(newValue) {
+		var oldValue = this.level;
 		if(newValue < LEVEL_MIN || newValue > LEVEL_MAX) {
 			return oldValue;
 		}
 		
-		levels[vocation] = newValue;
+		this.level = newValue;
 		return newValue;
+	};
+
+	//スキルポイント合計
+	Monster.prototype.totalSkillPts = function() {
+		var total = 0;
+		for(var skillCategory in this.skillPts)
+			total += this.skillPts[skillCategory];
+		
+		return total;
+	};
+	
+	//現在のレベルに対するスキルポイント最大値
+	Monster.prototype.maxSkillPts = function() {
+		return skillPtsGiven[this.level];
+	};
+	
+	//スキルポイント合計に対する必要レベル取得
+	Monster.prototype.requiredLevel = function() {
+		var total = this.totalSkillPts();
+		
+		for(var l = LEVEL_MIN; l <= LEVEL_MAX; l++) {
+			if(skillPtsGiven[l] >= total)
+				return l;
+		}
+		return NaN;
+	};
+	
+	//モンスター・レベルによる必要経験値
+	Monster.prototype.requiredExp = function(level) {
+		return expRequired[this.data.expTable][level];
+	};
+	
+	//不足経験値
+	Monster.prototype.requiredExpRemain = function() {
+		var required = this.requiredLevel();
+		if(required <= this.level) return 0;
+		var remain = this.requiredExp(required) - this.requiredExp(this.level);
+		return remain;
+	};
+
+	/* メソッド */
+
+	//モンスター追加
+	function addMonster (monsterType) {
+		if(monsters.length >= MONSTER_MAX)
+			return false;
+
+		monsters.push(new Monster(monsterType));
 	}
 
+	//API
+	return {
+		addMonster: addMonster,
+		monsters: monsters
+	};
 })();
+
