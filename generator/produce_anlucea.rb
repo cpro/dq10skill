802fd46d05ab9d3a5312c1fc6c2234ca6c50362d
s w <cpro2918@gmail.com>
2015-01-13 16:18:31 +0900

    モンスター用・アンルシア用についても広告分離を適用、各生成スクリプトから広告更新部分を除去

--- generator/produce_anlucea.rb
+++ generator/produce_anlucea.rb
@@ -3,14 +3,6 @@
 
 dir = File.dirname(__FILE__)
 
-refresh_item_cache = false
-
-#オプション解析: -aオプション時商品キャッシュのリフレッシュ
-require 'optparse'
-opts = OptionParser.new
-opts.on('-a') {|v| refresh_item_cache = true}
-opts.parse!(ARGV)
-
 if ARGV.size < 1
   index_html_path = "#{dir}/../anlucea.html"
 else
@@ -21,24 +13,9 @@ require 'rubygems'
 require 'haml'
 require 'json'
 
-ITEM_CACHE_PATH = "#{dir}/amazon_item_cache.json"
-
-item_list = []
-
-# -aオプション時、またはキャッシュが存在しない場合
-# AWSから商品タイトル・画像URL等を取得する
-if refresh_item_cache or !File.exist?(ITEM_CACHE_PATH)
-  system("ruby #{dir}/refresh_amazon_cache.rb")
-end
-
-#キャッシュをロード
-File.open(ITEM_CACHE_PATH, 'r') do |file|
-  item_list = JSON.parse(file.read, symbolize_names: true)
-end
-
 # スキル等データロード
 data = JSON.parse(File.read("#{dir}/../dq10skill-anlucea-data.json"))
 # HAMLからHTML生成
 File.open(index_html_path, 'w') do |file|
-  file.puts Haml::Engine.new(File.read("#{dir}/anlucea.haml")).render(Object.new, {:data => data, :item_list => item_list})
+  file.puts Haml::Engine.new(File.read("#{dir}/anlucea.haml")).render(Object.new, {:data => data})
 end
